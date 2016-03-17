b5ba06b248954e58bf3d7d458637976c7dbc153a
Selvyn Wright <swright@celestial.co.uk>
2016-03-17 13:07:54 +0000

    removed .bat files from ./dist folder, no longer needed

--- dist/run_SimpleCORBATimeServer.bat
+++
@@ -1,1 +0,0 @@
-java -jar SimpleCORBATimeServer.jar -ORBInitRef.NameService=corbaloc::localhost:2089/StandardNS/NameServer-POA/_root
--- dist/runjar_SimpleCORBATimeServer.bat
+++
@@ -1,3 +0,0 @@
-REM Use this bat file when the jacorb libraries are not part of the jar file.
-
-java -Xbootclasspath/p:lib\jacorb.jar;lib/logkit-1.2.jar;lib/wrapper-3.1.0.jar;lib/slf4j-jdk14-1.5.6.jar;lib/slf4j-api-1.5.6.jar -jar SimpleCORBATimeServer.jar -ORBInitRef.NameService=corbaloc::localhost:2089/StandardNS/NameServer-POA/_root
