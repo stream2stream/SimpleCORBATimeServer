b26c65a03a670d928defbaf1d7686907ebd69ec6
Selvyn Wright <swright@elestial.co.uk>
2020-01-31 13:22:01 +0000

    small housekeeping, no change to code base

--- build/classes/runjar_SimpleCORBATimeServer.bat
+++
@@ -1,3 +0,0 @@
-REM Use this bat file when the jacorb libraries are not part of the jar file.
-
-java -Xbootclasspath/p:lib\jacorb.jar;lib/logkit-1.2.jar;lib/wrapper-3.1.0.jar;lib/slf4j-jdk14-1.5.6.jar;lib/slf4j-api-1.5.6.jar -jar ./dist/SimpleCORBATimeServer.jar -ORBInitRef.NameService=corbaloc::%1:2089/StandardNS/NameServer-POA/_root -serverid=%2
